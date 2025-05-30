b46b93fd84e0d99dc750e8a78d8b807d2d610bff
Me1mori <ssalvarezleonardoaa@gmail.com>
2025-05-29 21:10:12 -0700

    Primera prueva

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("a1").onclick = function(){
+    window.location.href = "ecologista.html";
+}
+
+document.getElementById("a2").onclick = function(){
+    window.location.href = "pacifista.html";
+}
